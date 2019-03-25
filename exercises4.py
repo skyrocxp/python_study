32fa833aeba460af03faf1664c49edb4f42d4a01
skyrocxp <skyrocxp@163.com>
2019-03-25 20:06:25 +0800

    奇数练习

---
+++ exercises4.py
@@ -0,0 +1,14 @@
+# 编写一个程序，打印出0-100所有的奇数
+# 我的代码
+dig_list = range(1,101)
+for dig in dig_list:
+    if (dig + 1) % 2 == 0:
+        print(dig)
+    else:
+        pass
+
+# 老师的代码
+ls = range(1,101)
+for i in ls:
+    if i % 2 == 1:
+        print(i)
